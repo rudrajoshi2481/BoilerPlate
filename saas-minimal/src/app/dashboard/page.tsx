dcf93738c6f74d5cca20b5ad9d445dfef2f2b9a7
rudrajoshi2481 <rudrajoshi2481@gmail.com>
2024-08-01 15:48:29 +0530

    updated sidebar

--- saas-minimal/src/app/dashboard/page.tsx
+++ saas-minimal/src/app/dashboard/page.tsx
@@ -1,13 +1,17 @@
-
-import SidebarIndex from '@/components/web/Sidebar/SidebarIndex'
-import React from 'react'
+import SidebarIndex from "@/components/web/Sidebar/SidebarIndex";
+import React from "react";
 
 function Page() {
   return (
-    <div className='flex'>
-    <SidebarIndex />      
+    <div className="flex w-full">
+      <div className="relative">
+        <SidebarIndex />
+      </div>
+      <div className="flex w-full flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <div className="w-full bg-fuchsia-100"><p className="text-center">Dashboard</p></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
--- saas-minimal/src/app/page.tsx
+++ saas-minimal/src/app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function page() {
   return (
-    <div>Home Page</div>
+    <div className='w-full'>
+      <h1 className='text-2xl mt-2 font-bold   text-center'>Home Page</h1>
+    </div>
   )
 }
 
